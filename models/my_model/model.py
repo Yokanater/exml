afa9b2ca03f0074aad789656b1c723b120794a05
Hursh Bajaj <hurshbajaj2011@gmail.com>
2025-11-16 11:08:09 +0530

    cleanup

--- models/my_model/model.py
+++ models/my_model/model.py
@@ -1,5 +1,4 @@
 from env.controls import forward, back, steer_right, steer_left, boost, brake, reset
 
 def model(car):
-    back(car)
-    #print("Your code goes here!")
+    print("Your code goes here!")
